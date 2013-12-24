4641feb8a9c3804b861a846b75b75f031e347fd5
Kollin <kollinchu@gmail.com>
2013-12-24 15:46:44 +0800

    change dir.

--- worker.go
+++ worker.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/kollinchu/reife-worker/trello"
 )
 
 func main() {
 	for {
+		t := trello.NewTrello("111", "1111")
+		fmt.Println(t)
 		time.Sleep(12)
 		trello_key := os.Getenv("TRELLO_KEY")
 		fmt.Println(trello_key)
